b84ea781183bbf8b75f6988aedda6bc01efd4037
Mohammed Tanvir <tanvir.eatl@gmail.com>
2025-10-09 04:22:03 +0600

    sidebar reload issue fixed

---
+++ src/app/(job-seeker)/@sidebar/job-listings/[jobListingId]/page.tsx
@@ -0,0 +1,7 @@
+import JobBoardSidebar from "@/app/(job-seeker)/_shared/_JobBoardSidebar";
+
+const JobListingDetailsSiderbard = () => {
+  return <JobBoardSidebar />;
+};
+
+export default JobListingDetailsSiderbard;
