50db105373f54b65f92303c513d4308dd3c5b600
Mohammed Tanvir <tanvir.eatl@gmail.com>
2025-10-13 20:37:24 +0600

    ai-search page title added

--- src/app/(job-seeker)/ai-search/page.tsx
+++ src/app/(job-seeker)/ai-search/page.tsx
@@ -1,3 +1,5 @@
+import { Metadata } from "next";
+
 import { AsyncIf } from "@/components/AsyncIf";
 import LoadingSwap from "@/components/LoadingSwap";
 import {
@@ -10,6 +12,11 @@ import {
 import JobListingAISearchForm from "@/features/jobListings/components/JobListingAISearchForm";
 import { SignUpButton } from "@/services/clerk/components/AuthButtons";
 import { getCurrentUser } from "@/services/clerk/lib/getCurrentAuth";
+
+export const metadata: Metadata = {
+  description: "Anyone can search for new job openings using AI prompt.",
+  title: "AI Search | Job Board",
+};
 
 const AISearchPage = () => {
   return (
